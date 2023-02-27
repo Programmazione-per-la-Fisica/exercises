1d9946277615602a74e26a4962cd98f9c4360667
Simone Rossi Tisbeni <simone.rossitisbeni@unibo.it>
2023-02-27 13:21:48 +0100

    Add algorithmic solution

---
+++ exercises/smallest_of_N/algo.cpp
@@ -0,0 +1,12 @@
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <vector>
+
+int main() {
+  std::cout << "Insert a sequence of numbers (stop with Ctrl-D) \n";
+  std::vector<int> ivec(std::istream_iterator<int>(std::cin),
+                        std::istream_iterator<int>());
+  std::cout << "The smallest number entered is "
+            << *std::min_element(ivec.begin(), ivec.end()) << '\n';
+}
