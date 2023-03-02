2f6a653143cebe495232be74726ac652e2158e3e
SimonB00 <simone.balducci@gmail.com>
2023-03-02 13:00:30 +0100

    Draft of person class exercise

---
+++ person_class/person_class.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+class person{
+private:
+  std::string m_name;
+  std::string m_surname;
+public:
+  person(std::string const& name, std::string const& surname) 
+	: m_name{name}, m_surname{surname} {}
+
+  void name(std::string const& name) { m_name = name; }
+  std::string name() const { return m_name; }
+  void surname(std::string const& surname) { m_surname = surname; }
+  std::string surname() const { return m_surname; }
+};
