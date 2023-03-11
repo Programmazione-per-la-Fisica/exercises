a0320a5df118c43e1ab5d31d04acda783d1ff67c
SimonB00 <simone.balducci@gmail.com>
2023-03-11 18:39:20 +0100

    Formatted isPrime.cpp

--- exercises/isPrime/is_prime.cpp
+++ exercises/isPrime/is_prime.cpp
@@ -34,3 +34,41 @@ int main() {
   assert(isPrime(31) == true);
   std::cout << "TEST PASSED\n";
 }
+#include <cassert>
+#include <cmath>
+#include <iostream>
+
+bool isPrime(int num) {
+  for (int i{2}; i <= std::sqrt(num); ++i) {
+    if (num % i == 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+int main() {
+  assert(isPrime(2) == true);
+  std::cout << "TEST PASSED\n";
+  assert(isPrime(3) == true);
+  std::cout << "TEST PASSED\n";
+  assert(isPrime(5) == true);
+  std::cout << "TEST PASSED\n";
+  assert(isPrime(7) == true);
+  std::cout << "TEST PASSED\n";
+  assert(isPrime(11) == true);
+  std::cout << "TEST PASSED\n";
+  assert(isPrime(4) == false);
+  std::cout << "TEST PASSED\n";
+  assert(isPrime(6) == false);
+  std::cout << "TEST PASSED\n";
+  assert(isPrime(8) == false);
+  std::cout << "TEST PASSED\n";
+  assert(isPrime(10) == false);
+  std::cout << "TEST PASSED\n";
+  assert(isPrime(40) == false);
+  std::cout << "TEST PASSED\n";
+  assert(isPrime(31) == true);
+  std::cout << "TEST PASSED\n";
+}
