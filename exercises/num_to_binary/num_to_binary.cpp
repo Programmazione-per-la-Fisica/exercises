e3b3de6f1cf1f9ce06b7641a0f6a160c2486477c
SimonB00 <simone.balducci@gmail.com>
2023-03-11 11:22:19 +0100

    Added an exercise on the conversion of a number to binary

---
+++ exercises/num_to_binary/num_to_binary.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cassert>
+#include <string>
+#include <algorithm>
+
+std::string toBinary(int num) {
+  std::string binaryNum = "";
+
+  while (num != 0) {
+	binaryNum += std::to_string(num % 2);
+	num /= 2;
+  }
+  std::reverse(binaryNum.begin(), binaryNum.end());
+  return binaryNum;
+}
+
+int main() {
+  // Now we test the function for some numbers
+  assert(toBinary(2) == "10");
+  std::cout << "TEST PASSED\n";
+  assert(toBinary(3) == "11");
+  std::cout << "TEST PASSED\n";
+  assert(toBinary(4) == "100");
+  std::cout << "TEST PASSED\n";
+  assert(toBinary(5) == "101");
+  std::cout << "TEST PASSED\n";
+  assert(toBinary(10) == "1010");
+  std::cout << "TEST PASSED\n";
+  assert(toBinary(40) == "101000");
+  std::cout << "TEST PASSED\n";
+  assert(toBinary(100) == "1100100");
+  std::cout << "TEST PASSED\n";
+  assert(toBinary(101) == "1100101");
+  std::cout << "TEST PASSED\n";
+}
