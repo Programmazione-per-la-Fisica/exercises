e56508167cb25425f89e91574c85455be0767b37
SimonB00 <simone.balducci@gmail.com>
2023-03-13 17:25:20 +0100

    Changed variables in calculate_pi solution from float to double

--- exercises/calculate_pi/calculate_pi.cpp
+++ exercises/calculate_pi/calculate_pi.cpp
@@ -1,10 +1,10 @@
 #include <cmath>
 #include <iostream>
 
-float calculatePi(int n) {
-  float dx = 1. / n;
+double calculatePi(int n) {
+  double dx = 1. / n;
 
-  float result = 0.;
+  double result = 0.;
   for (int i{}; i < n; ++i) {
     result += dx * 4. / (1 + std::pow(i * dx, 2));
   }
@@ -13,7 +13,7 @@ float calculatePi(int n) {
 
 int main() {
   std::cout << "Write the number of intervals in which the interval [0,1] will "
-               "be divided: \n";
+               "be divided: ";
   int n;
   std::cin >> n;
   std::cout << calculatePi(n) << std::endl;
