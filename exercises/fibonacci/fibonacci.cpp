fb5757bc9d1c0006d7ef1d7c901904070dabd90b
sbaldu <simone.balducci00@gmail.com>
2023-08-22 12:43:02 +0200

    Fixes in fibonacci exercise

--- exercises/fibonacci/fibonacci.cpp
+++ exercises/fibonacci/fibonacci.cpp
@@ -4,10 +4,10 @@
 #include "../../doctest.h"
 
 int fibonacci(int n) {
-  if (n < 1) {
-    std::cout << "n must be at least one\n";
-    return -1;
-  } else if (n == 1 || n == 2) {
+  // n is the position in the Fibonacci sequence
+  assert(n >= 1);
+
+  if (n == 1 || n == 2) {
     return 1;
   } else {
     return fibonacci(n - 1) + fibonacci(n - 2);
@@ -17,15 +17,10 @@ int fibonacci(int n) {
 TEST_CASE(
     "Testing the function calculating the nth element of the fibonacci "
     "sequence") {
+  CHECK(fibonacci(1) == 1);
   CHECK(fibonacci(2) == 1);
   CHECK(fibonacci(3) == 2);
   CHECK(fibonacci(4) == 3);
-  CHECK(fibonacci(5) == 5);
-  CHECK(fibonacci(6) == 8);
-  CHECK(fibonacci(7) == 13);
-  CHECK(fibonacci(8) == 21);
   CHECK(fibonacci(9) == 34);
   CHECK(fibonacci(20) == 6765);
-  CHECK(fibonacci(0) == -1);
-  CHECK(fibonacci(-5) == -1);
 }
