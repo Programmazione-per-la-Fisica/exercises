3cb63c7ca91d4b79acbdb934d4a03a26c5e4af11
Simone Balducci <simone.balducci00@gmail.com>
2024-09-25 10:07:58 +0200

    Formatting

--- find_the_bug/pointDistance/solution/pointDistance.cpp
+++ find_the_bug/pointDistance/solution/pointDistance.cpp
@@ -3,40 +3,29 @@
 
 template <typename T>
 class Point {
-    private:
-        T m_x;
-        T m_y;
+ private:
+  T m_x;
+  T m_y;
 
-    public:
-        Point() = default;
-        Point(T x, T y) : m_x{x}, m_y{y} {}
+ public:
+  Point() = default;
+  Point(T x, T y) : m_x{x}, m_y{y} {}
 
-        // Getters
-        T x() const {
-            return m_x;
-		}
-        T y() const {
-            return m_y;
-        } 
-
-        // Setters
-        void set_x(T x) {
-            m_x = x;
-        }
-        void set_y(T y) {
-            m_y = y;
-        }
+  // Getters
+  T x() const { return m_x; }
+  T y() const { return m_y; }
 };
 
 template <typename T>
 double distance(const Point<T>& p1, const Point<T>& p2) {
-    return sqrt(pow(p1.x() - p2.x(), 2) + pow(p1.y() - p2.y(), 2));
+  // NOTE: could also use std::hypot
+  return std::sqrt(std::pow(p1.x() - p2.x(), 2) + std::pow(p1.y() - p2.y(), 2));
 }
 
 int main() {
-    Point<int> p1{2, 3};
-    Point<int> p2{4, 1};
+  Point<int> p1{2, 3};
+  Point<int> p2{4, 1};
 
-    std::cout << "The result is: " << distance(p1, p2) << '\n';
-    std::cout << "The expected result is: " << 2*sqrt(2) << '\n';
+  std::cout << "The result is: " << distance(p1, p2) << '\n';
+  std::cout << "The expected result is: " << 2 * std::sqrt(2) << '\n';
 }
--- find_the_bug/vectorAccumulation/solution/vectorAccumulation.cpp
+++ find_the_bug/vectorAccumulation/solution/vectorAccumulation.cpp
@@ -3,10 +3,10 @@
 #include <vector>
 
 double return_sum(const std::vector<double>& vec) {
-    return std::accumulate(vec.begin(), vec.end(), 0.);
+  return std::accumulate(vec.begin(), vec.end(), 0.);
 }
 
 int main() {
-    std::cout << "Expected result: " << 16.5 << '\n';
-    std::cout << "Obtained result: " << return_sum({3.5, 10.4, 2.6}) << '\n';
+  std::cout << "Expected result: " << 16.5 << '\n'
+            << "Obtained result: " << return_sum({3.5, 10.4, 2.6}) << '\n';
 }
