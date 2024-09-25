ac4df0473a18c7f6d8a07ee5efa6b0d8092088d5
Simone Balducci <simone.balducci00@gmail.com>
2024-09-25 10:07:31 +0200

    Use `auto` and remove setters

--- find_the_bug/rectangleArea/solution/rectangleArea.cpp
+++ find_the_bug/rectangleArea/solution/rectangleArea.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
 
 class rectangle {
-    private:
-        double m_base;
-        double m_height;
-    public:
-        rectangle(double base=0., double height=0.) : m_base{base}, m_height{height} {}
+ private:
+  double m_base;
+  double m_height;
 
-		// Getters
-		double base() const { return m_base; }
-		double height() const { return m_height; }
+ public:
+  rectangle(double base, double height) : m_base{base}, m_height{height} {}
+
+  // Getters
+  auto base() const { return m_base; }
+  auto height() const { return m_height; }
 };
 
-double calculateArea(const rectangle& rect) {
-    return rect.base() * rect.height();
+auto calculateArea(const rectangle& rect) {
+  return rect.base() * rect.height();
 }
 
 int main() {
-    rectangle r{4., 5.};
-    std::cout << "The area is: " << calculateArea(r) << '\n';
+  rectangle r{4., 5.};
+  std::cout << "The area is: " << calculateArea(r) << '\n';
 }
