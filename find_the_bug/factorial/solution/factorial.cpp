1bb942a2a284e5955387a553f11c21eb7ff26ca9
Simone Balducci <simone.balducci00@gmail.com>
2024-09-25 10:07:45 +0200

    Use ternary operator

--- find_the_bug/factorial/solution/factorial.cpp
+++ find_the_bug/factorial/solution/factorial.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 
 long long int factorial(long long int x) {
-    if (x == 0 || x == 1) {
-        return 1;
-    } else {
-        return x * factorial(x-1);
-    }
+  assert(x >= 0);
+
+  // rewrite with ternary operator
+  if (x == 0 || x == 1) ? return 1 : return x * factorial(x - 1);
 }
 
 int main() {
-    std::cout << "Result: " << factorial(13) << '\n';
-    std::cout << "Expected result: " << 6227020800 << std::endl;
+  std::cout << "Result: " << factorial(13) << '\n'
+            << "Expected result: " << 6227020800 << '\n';
 }
