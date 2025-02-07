2175dada27e088c8e623ad77569d01573e2ca123
Carlo Battilana <carlo.battilana@cern.ch>
2025-02-07 17:27:33 +0100

    Fix "vexing parse"-related compilation issue arising when using clang

--- problemi/smallest_of_N/algo.cpp
+++ problemi/smallest_of_N/algo.cpp
@@ -5,8 +5,8 @@
 
 int main() {
   std::cout << "Insert a sequence of numbers (stop with Ctrl-D) \n";
-  std::vector<int> ivec(std::istream_iterator<int>(std::cin),
-                        std::istream_iterator<int>());
+  std::vector<int> ivec{std::istream_iterator<int>{std::cin},
+                        std::istream_iterator<int>{}};
   std::cout << "The smallest number entered is "
             << *std::min_element(ivec.begin(), ivec.end()) << '\n';
 }
